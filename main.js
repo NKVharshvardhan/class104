a80caa0c9c86cd238dfa77deace53076499fb9e9
NKVharshvardhan <73694587+NKVharshvardhan@users.noreply.github.com>
2021-04-19 13:12:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,12 +8,8 @@ Webcam.set({
 var camera=document.getElementById("camera");
 Webcam.attach('#camera');
 
-function take_snapshot(){
-    Webcam.snap(function(data_uri)
-   {
  document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';      
     
-});
-}   
+ 
 
 
